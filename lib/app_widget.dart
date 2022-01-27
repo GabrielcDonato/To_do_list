21e584bdc91423059f9bb4f08d6ce92dc8289f7c
Gabriel Donato <gabrielcdonato@gmail.com>
2022-01-26 21:15:31 -0300

    initial commit

---
+++ lib/app_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'home/presentation/pages/home_page.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: const HomePage(),
+    );
+  }
+}
